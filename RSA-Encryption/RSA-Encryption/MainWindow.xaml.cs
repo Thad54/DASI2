ea2a8d7187ee6d66b8ca30dd24ee5e3a975cb292
dreydn <if12b037@technikum-wien.at>
2014-03-04 10:59:13 +0100

    project created

---
+++ RSA-Encryption/RSA-Encryption/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace RSA_Encryption
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string input;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+           /// dlg.Filter = "Text documents (.txt|*.txt)";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                FileTextBox.Text = filename;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            RSAParameters RSAKeyInfo = rsa.ExportParameters(false);
+
+            //string input = 
+
+
+        }
+
+
+    }
+}
