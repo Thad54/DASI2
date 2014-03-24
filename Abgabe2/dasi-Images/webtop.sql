ab5e00dae57f48b7c1ac62ae1ccfad66463456ed
dreydn <if12b037@technikum-wien.at>
2014-03-24 12:28:13 +0100

    added database script

---
+++ Abgabe2/dasi-Images/webtop.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 23, 2014 at 08:59 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `webtop3`
+--
+Create Database webtop;
+use webtop;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(32) NOT NULL,
+  `pwd` varchar(32) DEFAULT NULL,
+  `vorname` varchar(32) DEFAULT NULL,
+  `nachname` varchar(32) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `picture` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
